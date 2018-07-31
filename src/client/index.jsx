64e7548c90459fb3ff5bd8ec2af3cf2a2d0a3267
davalapar <davalapar@gmail.com>
2018-07-31 18:24:50 +0800

    lints

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,12 +4,10 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import swal from 'sweetalert2';
 
-const App = () => {
-  return (
-    <Button variant="contained" color="primary" onClick={() => swal('Clicked!')}>
-      Hello World
-    </Button>
-  );
-}
+const App = () => (
+  <Button variant="contained" color="primary" onClick={() => swal('Clicked!')}>
+    Hello World
+  </Button>
+);
 
 ReactDOM.render(<App />, document.querySelector('#app'));
